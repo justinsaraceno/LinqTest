fa18037bf46b0bd5a2c31ea41cac7115e6372c14
justin Saraceno <justin.saraceno@gmail.com>
2012-10-18 12:08:39 -0400

    iniital commit

---
+++ LinqTest/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinqTest.Controllers
+{
+    using LinqToExcel;
+
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formCollection)
+        {
+            var excel = new ExcelQueryFactory
+            {
+                FileName = Server.MapPath("~/App_Data/testExcel.xls")
+            };
+
+            return this.View();
+        }
+    }
+}
